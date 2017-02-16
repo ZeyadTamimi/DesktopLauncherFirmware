56f3be57ff168e446ae6eb16fdbe99112ffb829e
Nicholas Wu <nickwu241@gmail.com>
2017-02-15 23:34:28 -0800

    Add new motor code including firing motor

--- Desktop_Launcher/motor.c
+++ Desktop_Launcher/motor.c
@@ -1,20 +1,25 @@
-//===================================================================
-// Includes
-//===================================================================
-// System Includes
 #include <assert.h>
-// project Includes
+
 #include "motor.h"
 
 //===================================================================
-// Defines
+// Memory Mapped Defines
 //===================================================================
-#define PWM_CW1 (*((unsigned char*) 0x80000010))
-#define PWM_CW2 (*((unsigned char*) 0x80000000))
+#define PWM_CW180 (*((unsigned short*) 0x80000010)) // u/d GPIO 40
+#define PWM_CW360 (*((unsigned short*) 0x80000000)) // l/r GPIO 38
+#define PWM_CWFIRE (*((unsigned short*) 0x80000060)) // fire GPIO 36
 
-#define kMinMotor 10 // min - right (1ms / 20ms)
-#define kMaxMotor 33 // max - left  (2ms / 20ms)
+//===================================================================
+// Motor Limits
+//===================================================================
+#define kCW180_MAX 7500
+#define kCW180_MIN 1850
+#define kCW180_MID (( kCW180_MIN + kCW180_MAX ) / 2)
 
+#define kRANGE360 1000
+#define kCW360_MID 4150
+#define kCW360_MAX (kCW360_MID + kRANGE360)
+#define kCW360_MIN (kCW360_MID - kRANGE360)
 
 //===================================================================
 // Constants
@@ -23,64 +28,108 @@
 //===================================================================
 // Global variables
 //===================================================================
-unsigned char motor_speed;
+unsigned short updown_speed;
+unsigned short leftright_speed;
 
 //===================================================================
 // Private Function Definitions
 //===================================================================
+int motor_test()
+{
+	init_motors();
+	while (1)
+	{
+		/*
+		unsigned char c;
+		printf("enter: ");
+		scanf("%c", &c);
+		if (c == 'w') move_up();
+		else if (c == 'a') move_left();
+		*/
 
+		int pwm_counter;
+		printf("enter: ");
+		scanf("%d", &pwm_counter);
+
+	}
+	return 0;
+}
 //===================================================================
 // Public Function Definitions
 //===================================================================
 void init_motors(void)
 {
-	motor_speed = 0x01;
-	PWM_CW1 = (kMinMotor + kMaxMotor ) / 2;
-	PWM_CW2 = (kMinMotor + kMaxMotor ) / 2;
+    set_motor_speed(1);
+    PWM_CW180 = kCW180_MID;
+    PWM_CW360 = kCW360_MID;
+    PWM_CWFIRE = kCW180_MID;
 }
 
 void move_up(void)
 {
-	if (PWM_CW1 < kMaxMotor)
+    if (PWM_CW180 < kCW180_MAX)
     {
-        PWM_CW1 += motor_speed;
-        PWM_CW1 = PWM_CW1 < kMaxMotor ? PWM_CW1 : kMaxMotor;
-        assert(PWM_CW1 <= kMaxMotor);
+        PWM_CW180 += updown_speed;
+        PWM_CW180 = PWM_CW180 < kCW180_MAX ? PWM_CW180 : kCW180_MAX;
+        assert(PWM_CW180 <= kCW180_MAX);
     }
 }
 
 void move_down(void)
 {
-    if (PWM_CW1 > kMinMotor)
+    if (PWM_CW180 > kCW180_MIN)
     {
-        PWM_CW1 -= motor_speed;
-        PWM_CW1 = PWM_CW1 > kMinMotor ? PWM_CW1 : kMinMotor;
-        assert(PWM_CW1 >= kMinMotor);
+        PWM_CW180 -= updown_speed;
+        PWM_CW180 = PWM_CW180 > kCW180_MIN ? PWM_CW180 : kCW180_MIN;
+        assert(PWM_CW180 >= kCW180_MIN);
     }
 }
 
 void move_left(void)
 {
-    if (PWM_CW2 < kMaxMotor)
+    if (PWM_CW360 < kCW360_MAX)
     {
-        PWM_CW2 += motor_speed;
-        PWM_CW2 = PWM_CW2 < kMaxMotor ? PWM_CW2 : kMaxMotor;
-        assert(PWM_CW2 <= kMaxMotor);
+        PWM_CW360 += leftright_speed;
+        PWM_CW360 = PWM_CW360 < kCW360_MAX ? PWM_CW360 : kCW360_MAX;
+        assert(PWM_CW360 <= kCW360_MAX);
     }
 }
 
 void move_right(void)
 {
-    if (PWM_CW2 > kMinMotor)
+    if (PWM_CW360 > kCW360_MIN)
     {
-        PWM_CW2 -= motor_speed;
-        PWM_CW2 = PWM_CW2 > kMinMotor ? PWM_CW2 : kMinMotor;
-        assert(PWM_CW1 >= kMinMotor);
+        PWM_CW360 -= leftright_speed;
+        PWM_CW360 = PWM_CW360 > kCW360_MIN ? PWM_CW360 : kCW360_MIN;
+        assert(PWM_CW360 >= kCW360_MIN);
     }
 }
 
-void set_motor_speed(void* speed)
+void stop_leftrght(void)
 {
-	motor_speed = *((unsigned char*)speed);
-	assert(MIN_MOTOR_SPEED <= motor_speed && motor_speed <= MAX_MOTOR_SPEED);
+    PWM_CW360 = kCW360_MID;
+}
+
+void motor_load(void)
+{
+    PWM_CWFIRE = kCW180_MID;
+}
+
+void motor_fire(void)
+{
+    PWM_CWFIRE = kCW180_MAX;
+}
+
+void set_motor_speed(int speed_multiplier)
+{
+    updown_speed = 50 * speed_multiplier;
+    leftright_speed = 10 * speed_multiplier;
+}
+
+// Use this to debug!
+void set_direct_PWM(int motor, int pwm_counter)
+{
+    if (motor == 0) PWM_CW180 = pwm_counter;
+    else if (motor == 1) PWM_CW360 = pwm_counter;
+    else if (motor == 2) PWM_CWFIRE = pwm_counter;
 }
--- Desktop_Launcher/motor.h
+++ Desktop_Launcher/motor.h
@@ -2,13 +2,23 @@
 #define MOTOR_H_
 
 #define MIN_MOTOR_SPEED 1
-#define MAX_MOTOR_SPEED 3
+#define MAX_MOTOR_SPEED 9
 
 void init_motors(void);
 void move_up(void);
 void move_down(void);
 void move_left(void);
 void move_right(void);
-void set_motor_speed(void*);
+void stop_leftrght(void);
+void motor_load(void);
+void motor_fire(void);
+// use around 1 - 9
+void set_motor_speed(int speed_multiplier);
+
+// Use this to debug
+// motor - 0 for updown
+//         1 for l/r 360,
+//         2 for fire
+void set_direct_PWM(int motor, int pwm_counter);
 
 #endif /* MOTOR_H_ */
