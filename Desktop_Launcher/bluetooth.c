e8726d2cdfae73ebe8f13449332d892c95ba6aa3
Konrad I <konrad.i.1996@gmail.com>
2017-03-13 22:21:58 -0700

    completed bluetooth library

--- Desktop_Launcher/bluetooth.c
+++ Desktop_Launcher/bluetooth.c
@@ -2,28 +2,152 @@
 // Includes
 //===================================================================
 #include <stdint.h>
+#include <unistd.h>
+#include <string.h>
 
-#include "serial.h"
 #include "bluetooth.h"
+#include "serial.h"
+#include "debug.h"
 //===================================================================
 // Defines
 //===================================================================
 
 
 //===================================================================
+// Private Function Definitions
+//===================================================================
+void get_cmd_response(size_t size)
+{
+	serial_read(BLUETOOTH, in_buffer, size);
+	in_buffer[size] = '\0';
+	printf("[%s]\n", in_buffer);
+}
+
+void flush_buffer(void)
+{
+	serial_read_timeout(BLUETOOTH, in_buffer, BUFFER_SIZE, 10);
+	memset(in_buffer, 0, 256);
+}
+
+void enter_cmd_mode(void)
+{
+	printf("enter command mode");
+	serial_write(BLUETOOTH, "$$$", 3);
+	get_cmd_response(5);
+}
+
+void exit_cmd_mode(void)
+{
+	printf("exit command mode");
+	serial_write(BLUETOOTH, "---\r\n", 5);
+	get_cmd_response(5);
+}
+
+
+void set_name(void)
+{
+	printf("bluetooth: set name");
+	serial_write(BLUETOOTH, "SN,DTL\r\n", 8);
+	get_cmd_response(5);
+}
+
+void set_pin(void)
+{
+	printf("bluetooth: set auth mode");
+	serial_write(BLUETOOTH, "SA,4\r\n", 6);
+	get_cmd_response(5);
+	usleep(1000);
+	printf("bluetooth: set pin");
+	serial_write(BLUETOOTH, "SP,9999\r\n", 9);
+	get_cmd_response(5);
+}
+
+int check_connection(void)
+{
+	printf("bluetooth: get status");
+	serial_write(BLUETOOTH, "GK\r\n", 4);
+	get_cmd_response(7);
+
+	if(in_buffer[0] == '1')
+		return 1;
+	else
+		return 0;
+}
+
+
+//===================================================================
 // Public Function Definitions
 //===================================================================
+void bluetooth_main(void)
+{
+	printf("bluetooth alive\n");
+	init_bluetooth();
+
+	char c = 0;
+
+	while(1){
+		scanf("%c", &c);
+		printf("connected: [%d]", bluetooth_connected());
+	}
+}
+
 void init_bluetooth(void)
 {
+	flush_buffer();
 	init_serial(BLUETOOTH);
+	enter_cmd_mode();
+	usleep(1000);
+	set_name();
+	usleep(1000);
+	set_pin();
+	usleep(1000);
+	exit_cmd_mode();
 }
 
-int bluetooth_receive_message_timeout(uint8_t message_buffer, unsigned long timeout)
+int bluetooth_receive_message_timeout(uint8_t **message_buffer, unsigned long timeout)
 {
-	return 0;
+	uint16_t read_bytes;
+	read_bytes = serial_read_timeout(BLUETOOTH, in_buffer, 3, timeout);
+	if(read_bytes != 3) {
+		return 0;
+	}
+
+	uint16_t size = in_buffer[1]<<8 | in_buffer[2];
+	serial_read(BLUETOOTH, in_buffer, size);
+	*message_buffer = in_buffer;
+	return 1;
 }
 
-int bluetooth_send_response(uint8_t response_message, uint8_t response_code)
+void bluetooth_send_response(uint8_t response_message_id, uint8_t response_code)
 {
-	return 0;
+	uint8_t out_buffer[5];
+	out_buffer[0] = RESPONSE_ID;
+	out_buffer[1] = 0;
+	out_buffer[2] = 2;
+	out_buffer[3] = response_message_id;
+	out_buffer[4] = response_code;
+	serial_write(BLUETOOTH, out_buffer, 5);
 }
+
+void bluetooth_send_image(uint8_t *image_data, uint16_t image_size)
+{
+	uint8_t out_buffer[3];
+	out_buffer[0] = IMAGE_ID;
+	out_buffer[1] = image_size>>8;
+	out_buffer[2] = image_size & 0xFF;
+	serial_write(BLUETOOTH, out_buffer, 3);
+	serial_write(BLUETOOTH, image_data, image_size);
+}
+
+int bluetooth_connected(void)
+{
+	int result;
+
+	enter_cmd_mode();
+	usleep(1000);
+	result = check_connection();
+	exit_cmd_mode();
+
+	return result;
+}
+
--- Desktop_Launcher/bluetooth.h
+++ Desktop_Launcher/bluetooth.h
@@ -8,8 +8,10 @@
 #define MOVE_COMMAND_ANGLE_ID 0x02
 #define MOVE_COMMAND_CHANGE_SPEED 0x03
 #define IMAGE_REQUEST 0x10
+#define RESPONSE_ID 0xF1
+#define IMAGE_ID 0xF2
 
-// FIeld Sizes
+// Field Sizes
 #define MESG_FIELD_ID_SIZE 1
 #define MESG_FIELD_LENGTH_SIZE 2
 #define MESG_FIELD_HEADER_SIZE MESG_FIELD_ID_SIZE+MESG_FIELD_LENGTH_SIZE
@@ -21,9 +23,15 @@
 // Message Lengths
 #define MOVE_COMMAND_TIME_LENGTH 4
 
+#define BUFFER_SIZE 256
+unsigned char in_buffer[BUFFER_SIZE];
+
+void bluetooth_main(void);
 void init_bluetooth(void);
-int bluetooth_receive_message_timeout(uint8_t message_buffer, unsigned long timeout);
-int bluetooth_send_response(uint8_t response_message, uint8_t response_code);
+int bluetooth_receive_message_timeout(uint8_t **message_buffer, unsigned long timeout);
+void bluetooth_send_response(uint8_t response_message_id, uint8_t response_code);
+void bluetooth_send_image(uint8_t *image_data, uint16_t image_size);
+int bluetooth_connected(void);
 
 
 #endif /* BLUETOOTH_H_ */
